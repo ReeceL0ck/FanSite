4d9b34f1091c277d35c2cf82c16819453a444e82
Reee Lock <reece.lock55@gmail.com>
2025-11-01 23:05:57 +0000

    Added templates

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
+from fastapi.templating import Jinja2Templates
+
 # app = FastAPI(dependencies=[Depends(get_query_token)]) # Use this if we want all routes to have the dependency
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 app.include_router(users.router)
@@ -21,5 +28,7 @@ app.include_router(
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="mainplay.html", context={"id": id}
+    )
